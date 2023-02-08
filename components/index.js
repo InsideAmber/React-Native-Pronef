ee16e03a5c6cdc81ab53a0e796f137f86ec5c0d3
InsideAmber <lostinthoughtshard@gmail.com>
2023-02-08 16:30:50 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,30 @@
+import FocusedStatusBar from './FocusedStatusBar';
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import {
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+} from './SubInfo';
+
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  DetailsBid,
+  DetailsDesc,
+};
